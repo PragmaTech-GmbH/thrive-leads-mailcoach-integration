a8f2a2a59a5ef39a5ec2edf4d7c8b550c6415de8
Philip Riecks <mail@philipriecks.de>
2025-05-05 10:14:50 +0200

    fixes

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,9 @@
 <?php
-// Output PHP information
+/**
+ * Outputs PHP configuration information
+ *
+ * @package Thrive_Leads_Mailcoach_Integration
+ */
+
+// Output PHP information for development purposes only.
 phpinfo();
